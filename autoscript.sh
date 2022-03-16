b39aef8a6897db8fc444eb2dbdea45207822ed3b
renanlimasrc <eduardo.renan02@hotmail.com>
2022-03-16 10:20:08 -0300

    versao 0.3

--- autoscript.sh
+++ autoscript.sh
@@ -6,22 +6,34 @@ header="#!/bin/bash
 #-----------------------------------------------------
 # Script	: 
 # Descricao	: 
-# Versao	: 0.2
+# Versao	: 0.3
 # Autor		: n1nr0d eduardo.renan02@hotmail.com
 # Data		: $data
 # Licenca	: GPL 2.0
 #-----------------------------------------------------
-# Uso:
+# Uso: 
 #-----------------------------------------------------
 "
+#-----------------------------------------------------
+# Este script serve para gerar outro script.
+# Ele gera com nome .sh no final,
+# e add permissões de execução
+# O nome a ser chamado depois do ./autoscript, 
+# será o nome do script 
+# Exemplo: ./autoscript scriptteste
+# Logo em seguida, ele entra no editor vi
+#-----------------------------------------------------
+
 
 # Testar se há um nome para o script:
 
 [[ $# -ne 1 ]] && echo "Precisa de *um* nome e somente *um*!" && exit 1
 
+
 # Testar se o arquivo já existe:
 
 [[ -f $1 ]] && echo "Arquivo com esse nome já existe!Saindo..." && exit 1
+
 
 echo "$header" > "$1.sh"
 chmod +x "$1.sh"
